1c597502b43d254875aa6f024a3608600de4459f
sinkcup <sinkcup@gmail.com>
2019-04-17 14:13:48 +0800

    new: circleci phpunit

---
+++ phpunit.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+STARTED_AT=$(date +%s)
+./vendor/bin/phpunit --version
+for i in patches/*.patch; do patch -N -p0 -i $i || true; done
+php artisan migrate
+php artisan migrate:refresh
+if [ $? -ne 0 ]; then
+    exit 1
+fi
+I=1
+FLAG=0
+for FILENAME in $(find tests/ -type f -name "*Test.php")
+do
+    printf '-'
+    I=$(($I + 1))
+    OUTPUT=$(./vendor/bin/phpunit $FILENAME)
+    if [ $? -ne 0 ]; then
+        FLAG=$(($FLAG + 1))
+        printf "\n$FILENAME\n"
+        echo "$OUTPUT"
+        exit 1
+    fi
+done
+
+printf "\n\n"
+if [ $FLAG -eq 0 ]; then
+    echo 'OK ('$I' files)'
+else
+    echo 'ERRORS!'
+    echo 'Files: '$I', Errors: '$FLAG'.'
+fi
+FINISHED_AT=$(date +%s)
+echo 'Time taken: '$(($FINISHED_AT - $STARTED_AT))' seconds'
+exit $FLAG
