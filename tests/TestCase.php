8e136373d6fe5fe4d85a4055f20d9861b21193d8
sinkcup <sinkcup@gmail.com>
2019-04-17 14:38:46 +0800

    new: use redis in test

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,9 +2,26 @@
 
 namespace Tests;
 
+use Carbon\Carbon;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Support\Facades\Cache;
 
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
+    use WithFaker;
+
+    protected function setUp()
+    {
+        parent::setUp();
+        Carbon::setTestNow(now());
+        config(['cache.default' => 'redis']);
+    }
+
+    protected function tearDown()
+    {
+        Cache::flush();
+        parent::tearDown();
+    }
 }
